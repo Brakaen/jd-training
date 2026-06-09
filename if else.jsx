dc6611bb7de3fd42e79be6002a4f1f2a091fd047
Brakaen <brakaenn@gmail.com>
2026-06-09 14:46:39 +0530

    Add if-else example; update console messages
    Add a new if else.jsx example that logs different messages based on a speed variable. Change the switch statement default message from "Grade: F" to "You are failed". Remove an inline comment from the array and object file's console.log (minor formatting/message cleanup).

---
+++ if else.jsx
@@ -0,0 +1,11 @@
+let speed = 80;
+if (speed >=95) {
+    console.log("You are crossing the speed limit.");
+} else if  (speed >= 80) {
+    console.log("You are not speeding.");
+} 
+else if (speed >= 50) {
+    console.log("You are driving at a safe speed.");
+} else {
+    console.log("You are driving too slow.");
+}
